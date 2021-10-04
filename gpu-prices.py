676a8ad6ab3d395c7638066575bedab6e6509cbd
jacobmbaldwin <34068541+jacobmbaldwin@users.noreply.github.com>
2021-10-04 17:24:44 -0500

    Add files via upload
    Web scraper that grabs highest rated GPU names and prices from Newegg.

---
+++ gpu-prices.py
@@ -0,0 +1,33 @@
+import requests
+from bs4 import BeautifulSoup as soup
+from urllib.request import urlopen as uReq
+
+URL = "https://www.newegg.com/Desktop-Graphics-Cards/SubCategory/ID-48?Tid=7709&Order=4"
+#Opening up connection and getting the data
+uClient = uReq(URL)
+page_html = uClient.read()
+uClient.close()
+
+#html parsing
+page_soup = soup(page_html, "html.parser")
+
+containers = page_soup.findAll("div",{"class":"item-container"})
+#Write the list to csv
+filename = "products.csv"
+f = open(filename, "w")
+headers = "Product_Name, Product_Price\n"
+
+f.write(headers)
+
+#Loop to grab each name and price
+for container in containers:
+	name = container.findAll("a",{"class":"item-title"})
+	price = container.findAll("li",{"class":"price-current"})
+	product_name = name[0].text
+	product_price = price[0].text
+	print("Product_Name:" + product_name)
+	print("Product_Price:" + product_price)
+	
+	f.write(product_name.replace(",","|") + "," + product_price.replace(",","") + "\n")
+	
+f.close()
